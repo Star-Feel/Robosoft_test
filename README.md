895bfe50fde61cf8d01b0f32bf21b4412aabba9e
jobswei <3031864345@qq.com>
2025-05-13 09:12:12 +0000

    setup

--- README.md
+++ README.md
@@ -8,6 +8,7 @@ conda activate ssim
 pip install torch==2.5.1 torchvision==0.20.1 torchaudio==2.5.1 --index-url https://download.pytorch.org/whl/cu118
 pip install pyelastica
 pip install -r requirements.txt
+pip install -e.
 
 ```
 适配mesh surface
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='ssim',
+    version='0.1.0',
+    packages=find_packages(include=['ssim']),
+    install_requires=[],
+    description='ssim',
+    author='Ziyu Wei',
+    author_email='weiziyu99@gmail.com',
+)
